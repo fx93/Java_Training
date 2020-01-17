d6747ed58fc3e01f1b388035f42c0dc90eca2cc0
TKirushanth <tkirushanth13@gmail.com>
2020-01-17 06:59:33 +0530

    modified frog program

--- Frog.java
+++ Frog.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 
 public class Frog {
-
     public static void main(String[] args) {
-        /*Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("If you want to check the Distance with time, press D. Or vice versa, Press T: ");
         char option = scanner.next().toUpperCase().charAt(0);
         System.out.println();
@@ -12,66 +11,81 @@ public class Frog {
         System.out.println();
 
         if (option == 'D') {
-            Frog.calculateDistance(input);
+            if(input == 0){
+                System.out.println("Distance is: 0");
+            }
+            else{
+                Frog.calculateDistance(input);
+            }
         } else if (option == 'T') {
-            Frog.calculateTime(input);
+            if(input == 0){
+                System.out.println("Time is: 0");
+            }
+            else{
+                Frog.calculateTime(input);
+            }
         } else {
             System.out.println("Your input details were incorrect");
-        }*/
-
-        Frog.calculateDistance(53);
+        }
     }
 
-
     static void calculateTime(int d){
-        int cycle = 0;
+        int cycle = (d/9) + 1;
+        int t;
+        int difference = (9*cycle) - d;
 
-        if(cycle % 8 == 0){
-            cycle = (d/8);
+        // d vs t graph
+        //0-0,5-1,8-2,9-4,
+        //14-9,17-10,18-12,
+        //23-17,26-18,27-20
+
+        if(d % 9 == 0){
+            t = (8*(d/9)) - 4;
+            System.out.println("Time is: "+ t);
         }
-        else{cycle = (d/8) + 1;}
-        int interval = 8*cycle - d;
-
-        if (interval <= 4){
-            System.out.println("Least time is");
+        else{
+            if(difference<4 && difference>=1){
+                t = (8*cycle) - 6;
+                System.out.println("Time is: "+ t);
+            }
+            else {
+                t = (8*cycle) - 7;
+                System.out.println("Time is: "+ t);
+            }
         }
     }
 
     static void calculateDistance(int t){
-        int cycle = 0;
-        int d = 0;
+        int cycle;
+        int d;
 
-        if(cycle % 8 == 0){
+        // t vs d graph
+        //0-0,1-5,2-8,3-8,4-9,5.6.7.8-9
+        //....9-14,10-17,11-17,12-18,13.14.15.16-18
+
+        if(t % 8 == 0){
             cycle = (t/8);
         }
         else{
             cycle = (t/8) + 1;
         }
-
         int interval = 8*cycle - t;
 
-        System.out.println(cycle);
-        System.out.println(t);
-        System.out.println(interval);
-        System.out.println(56/8);
-        System.out.println(56%8);
 
-
-
-        /*if (interval <= 4){
+        if (interval <= 4){
             d = 9*cycle;
             System.out.println("Distance is: "+ d);
         }
         else if(interval <= 6 && interval > 4){
-            d = (9*(cycle-1)) + (8*cycle);
+            d = (9*(cycle-1)) + 8;
             System.out.println("Distance is: "+ d);
         }
         else if(interval == 7){
-            d = (9*(cycle-1)) + (5*cycle);
+            d = (9*(cycle-1)) + 5;
             System.out.println("Distance is: "+ d);
         }
         else{
             System.out.println("system is down");
-        }*/
+        }
     }
 }
