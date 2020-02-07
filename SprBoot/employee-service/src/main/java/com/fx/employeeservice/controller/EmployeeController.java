115eb1300517ad3f7ff13955a03d0235e5523e99
TKirushanth <tkirushanth13@gmail.com>
2020-02-07 07:43:14 +0530

    produce xml format results

--- SprBoot/employee-service/src/main/java/com/fx/employeeservice/controller/EmployeeController.java
+++ SprBoot/employee-service/src/main/java/com/fx/employeeservice/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.fx.employeeservice.controller;
 
 import com.fx.employeeservice.model.Employee;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class EmployeeController {
         return "Hello world from SpringBoot";
     }
 
-    @RequestMapping("/employee")
+    @RequestMapping(value = "/employee", produces = MediaType.APPLICATION_XML_VALUE)
     public List<Employee> getEmployees(){
         return Employee.getAllEmployees();
     }
